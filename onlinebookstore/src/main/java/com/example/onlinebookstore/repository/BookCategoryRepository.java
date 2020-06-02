51e01337e3e415f2c6e384aad1f8341d4b1d5d4d
yeison.oyola <yeison.oyola@SBBOGLAPPROJO71.sophos.col.com>
2020-06-01 21:25:32 -0500

    Search Books based on keyword

--- onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,6 +2,7 @@ package com.example.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.onlinebookstore.entity.BookCategory;
 
--- onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.onlinebookstore.entity.Book;
 
-@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 	//To override the rest end point name use @RestResource
 	@RestResource(path = "categoryid")
 	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	
+	@RestResource(path = "searchbykeyword")
+	Page<Book> findByNameContaining(@Param("name") String keyword, Pageable pageable);
 }
